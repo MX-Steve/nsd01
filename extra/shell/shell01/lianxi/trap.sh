63c6f7d4585da3aadb26b3d67128ec2ab7ec2976
MX-Steve <2513556525@qq.com>
2018-06-22 20:48:35 +0800

    nsd01-shell01-debug

--- extra/shell/shell01/lianxi/trap.sh
+++ extra/shell/shell01/lianxi/trap.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#定义trap函数，跟踪状态变化
 function exittrap(){
 	echo "$0 exit status $?"
 	#$0
 }
-#自定义函数
 function foo(){
 	echo is foo
 	return 1
 }
-#执行trap函数，跟踪捕捉ERR和EXIT状态变化
 trap 'exittrap' ERR EXIT
 
 
