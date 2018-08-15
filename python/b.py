bc95c8a9dd06a823645e781dd505263567a497b1
MX-Steve <2513556525@qq.com>
2018-08-15 10:26:30 +0800

    b.py

--- python/b.py
+++ python/b.py
@@ -22,4 +22,15 @@ print(py_str[2:4])
 print(py_str[2:])
 print(py_str[:4])
 print(py_str*2)
+print(py_str[::2]) #2为步长值
+print(py_str[1::2])#从1开始，2为步长值
+print(py_str[::-1])#-1为负值，从右往左取值
 print("===========定义列表=============")
+alist = [1,'tom','jack',[11,22,33]]
+r='tom' in alist
+r1=22 in alist
+r2=1 in alist
+print(r2)
+alist[1]="xixi"
+print(alist)
+print(alist[-1][1])
