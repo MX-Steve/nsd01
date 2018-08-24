0a4f9402191b955d157ce5aed09bc3c9985f91c9
MX-Steve <2513556525@qq.com>
2018-08-24 15:49:33 +0800

    mtping_oop

--- project/devops/day10/mt.py
+++ project/devops/day10/mt.py
@@ -1,1 +1,30 @@
 #多线程：包含在一个进程内，没有僵尸进程问题，共用进程资源
+import time
+import os
+
+def calc():
+    result = 0
+    for i in range(50000000):
+        result +=i
+    print(result)
+
+if __name__ == '__main__':
+    # start = time.time()
+    # calc()
+    # calc()
+    # end=time.time()
+    # print(end-start)
+    ####################################################
+    start = time.time()
+    pid=os.fork()
+    if not pid:
+        calc()
+        exit()
+    pid=os.fork()
+    if not pid:
+        calc()
+        exit()
+    os.waitpid(-1,0) #父挂起
+    os.waitpid(-1, 0)
+    end = time.time()
+    print(end-start)
---
+++ project/devops/day10/mtping_oop_2.py
@@ -0,0 +1,20 @@
+import subprocess
+import threading
+class Ping:
+    def __init__(self,host):
+        self.host=host
+    def __call__(self, *args, **kwargs):
+        result = subprocess.call(
+            'ping -c2 %s &>/dev/null' % self.host,
+            shell=True
+        )
+        if result == 0:
+            print("%s : up " % self.host)
+        else:
+            print("%s : down " % self.host)
+
+if __name__ == '__main__':
+    ips=['176.4.13.%s' % ip for ip in range(1,255)]
+    for ip in ips:
+        t=threading.Thread(target=Ping(ip))
+        t.start()
