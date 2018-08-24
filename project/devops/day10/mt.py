bc34c27d62a379598edcc932260ffe13c1b9f9e3
MX-Steve <2513556525@qq.com>
2018-08-24 19:53:26 +0800

    fork and thread

--- project/devops/day10/mt.py
+++ project/devops/day10/mt.py
@@ -1,10 +1,18 @@
 #多线程：包含在一个进程内，没有僵尸进程问题，共用进程资源
+
+#多进程
+#cpu多核心，一个进程在一个核心上，另外一个进程在第二个核心上
+
+#GIL:全局解释器锁
+#GIL的存在导致某一时刻只能把一个线程交给cpu处理
+
 import time
 import os
+import threading
 
-def calc():
+def calc(n=50000000):
     result = 0
-    for i in range(50000000):
+    for i in range(n):
         result +=i
     print(result)
 
@@ -15,16 +23,27 @@ if __name__ == '__main__':
     # end=time.time()
     # print(end-start)
     ####################################################
+    # start = time.time()
+    # pid=os.fork()
+    # if not pid:
+    #     calc()
+    #     exit()
+    # pid=os.fork()
+    # if not pid:
+    #     calc()
+    #     exit()
+    # os.waitpid(-1,0) #父挂起
+    # os.waitpid(-1, 0)
+    # end = time.time()
+    # print(end-start)
+    ####################################################
     start = time.time()
-    pid=os.fork()
-    if not pid:
-        calc()
-        exit()
-    pid=os.fork()
-    if not pid:
-        calc()
-        exit()
-    os.waitpid(-1,0) #父挂起
-    os.waitpid(-1, 0)
-    end = time.time()
+    n=10000
+    t1 = threading.Thread(target=calc,args=(n,))
+    t1.start()
+    t2=threading.Thread(target=calc,args=(n,))
+    t2.start()
+    t1.join()
+    t2.join() #线程中没有僵尸线程，只是挂起主线程，等到t1线程结束后再向下指向
+    end=time.time()
     print(end-start)
