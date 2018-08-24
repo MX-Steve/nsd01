a74f2ba84dde1d4aa11ab3bb73e9e51dc56c5782
MX-Steve <2513556525@qq.com>
2018-08-24 09:15:55 +0800

    socket

---
+++ project/day10/match.py
@@ -0,0 +1,11 @@
+#http://down.51cto.com  ==><正则表达式必知必会>
+import re
+
+# 正则表达式贪婪匹配
+data = 'my phone number : 1501234565'
+m = re.search('.+(\d+)', data)
+print(m.group())
+print(m.group(1))
+#默认+*都是贪婪匹配，在其后面加上？就让?右边的模式尽量多的匹配
+n=re.search('.+?(\d+)',data)
+print(n.group(1))
