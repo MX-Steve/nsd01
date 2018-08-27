69bd3c313f0c2ee9c024c5bfc073c1bb15e0e997
MX-Steve <2513556525@qq.com>
2018-08-27 18:01:04 +0800

    query_method

--- project/devops/day11/query1.py
+++ project/devops/day11/query1.py
@@ -1,18 +1,48 @@
 from dbconnetion import Departments,Employees,Salary,Session
 session=Session()
 
+# q 1
 #qset=session.query(Departments).order_by(Departments.dep_id)
 #print(qset)
 #for dep in qset: # 向qset取值时，得到一个个实例
 #	print("%s:%s"%(dep.dep_id,dep.dep_name))
 
+# q 2
 #qset=session.query(Employees.emp_name,Employees.phone)
 #print(qset)
-#for name,phone in qset:
+#for name,phone in qset: #qset执行后返回的是一个元祖
 #	print("%s:%s" %(name,phone))
-qset = session.query(Departments.dep_name.label('部门'))
+#qset = session.query(Departments.dep_name.label('部门'))
+#print(qset)
+#for row in qset:
+#	print(row.部门)
+
+#qset=session.query(Employees.emp_name,Employees.email).\
+#	order_by(Employees.emp_id)[3:6]  #[3:6]代表切片,切片后变成元祖组成的列表
+
+# qset = session.query(Employees.emp_name).filter(Employees.dep_id==2).filter(Employees.gender=="m")
+# print(qset)
+#for name in qset:
+#	print(name)
+
+#qset=session.query(Employees.emp_name).filter(Employees.emp_name.like("z%"))
+#print(qset)
+#for name in qset:
+#	print(name)
+
+#qset = session.query(Employees.emp_name).filter(Employees.emp_name.in_(['zhangsan','zhanghong']))
+#print(qset)
+#for name in qset:
+#	print(name)
+
+#qset = session.query(Employees.emp_name).filter(~Employees.emp_name.in_(['zhangsan','zhanghong']))
+#print(qset)
+#for name in qset:
+#        print(name)
+
+qset = session.query(Employees.emp_name).filter(Employees.emp_name.isnot(None))
 print(qset)
-for row in qset:
-	print(row.部门)
+for name in qset:
+	print(name)
 
 session.close()
