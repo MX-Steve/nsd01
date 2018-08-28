5d8224ff71370f7e3e66e6cd0c8a52d46f5d77ee
MX-Steve <2513556525@qq.com>
2018-08-28 11:24:00 +0800

    url_img_1

--- project/devops/day12/downloadimg.py
+++ project/devops/day12/downloadimg.py
@@ -1,9 +1,14 @@
+# 将爬取到的源码中的图片取出并下载
 from urllib_url import get_url
+# 用来爬取网站源码
 from urllib_2 import get_data
 import os
 
-url=r'http://[\w/.-]+\.(jpg|jpeg|png|gif)'
+# 获取源码文件
 get_data('http://www.tmooc.cn/','/tmp/bbb.html')
+# 编写图片正则
+url=r'http://[\w/.-]+\.(jpg|jpeg|png|gif)'
+# 从图片正则下载并存储图片
 urls=get_url(url,'/tmp/bbb.html')
 
 img_dir='/tmp/images'
@@ -11,6 +16,8 @@ if not os.path.exists(img_dir):
 	os.mkdir(img_dir)
 
 for url in urls:
-	fname=url.split('/')[-1]
-	fname=os.path.join(img_dir,fname)
-	get_data(url,fname)
+	fname=os.path.join(img_dir,url.split('/')[-1])
+	try:
+		get_data(url,fname)
+	except:
+		pass
