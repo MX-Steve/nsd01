f091374304415a62e536631e64d19813c6f81e9b
MX-Steve <2513556525@qq.com>
2018-08-30 10:53:16 +0800

    weather_display

--- project/devops/day13/weather.py
+++ project/devops/day13/weather.py
@@ -5,6 +5,13 @@ a=urlopen('http://www.weather.com.cn/data/sk/101010100.html')
 data=a.read()
 print(json.loads(data))
 a.close()
+# 中国天气网各城市代码weather_cityId
+# https://blog.csdn.net/u013193363/article/details/44851897
 
 # 图片位置
 # http://m.weather.com.cn/img/c0.gif
+
+# 中国天气网：www.weather.com.cn
+# www.weather.com.cn/data/zs/城市编号.html 城市指数
+# www.weather.com.cn/data/sk/城市编号.html 城市实况
+# www.weather.com.cn/data/cityinfo/城市编号.html 城市信息
---
+++ project/devops/day13/weather_display.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+import json
+
+def get_weather(city_code):
+	url="http://www.weather.com.cn/data/sk/%s.html" % city_code
+	html=urlopen(url)
+	data=html.read()
+	data=json.loads(data)
+	output="风向： %s ,风力：%s，温度：%s，湿度：%s" % (
+		data["weatherinfo"]['WD'],
+		data["weatherinfo"]['WS'],
+		data["weatherinfo"]['temp'],
+		data["weatherinfo"]['SD']
+	)
+	return output
+
+if __name__=="__main__":
+
+	city_codes={"0":"101010100","1":"101280101","2":"101210101"}
+	prompt="""(0) 北京
+(1) 广州
+(2) 杭州
+(3) 成都
+请选择(0/1/2/3):
+"""
+	choice=input(prompt)
+	print(get_weather(city_codes[choice]))
