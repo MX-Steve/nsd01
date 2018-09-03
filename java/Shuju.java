ea756ebd55b85e8a290355cad05435b44c78f40b
MX-Steve <2513556525@qq.com>
2018-09-03 21:52:21 +0800

    java-second

---
+++ java/Shuju.java
@@ -0,0 +1,22 @@
+public class Shuju
+{
+	public static void main(String args[]){
+		/*String name="李韩";
+		String money = "12.34元";
+		double a=10.2;
+		System.out.println(name+money);
+		System.out.println(money +a);
+		
+		//System.out.println(money-a);语法错误
+		//转义字符
+		System.out.println("你好\t：\"李四\"");
+		int a = 5;
+		int b = 7;
+		System.out.println((a+b)*(a-b));
+		*/
+		System.out.println(2>4 && 10/0 == 0);
+		//逻辑开发都使用&& 和||
+
+
+	}
+}
